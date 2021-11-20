49bd4c4b10bf93d2197d3915fbf9c5c981d043c3
jiahoe <mrjiahoe@hotmail.com>
2021-11-20 19:04:22 +0800

    add cart controller

---
+++ controllers/cartControllers.js
@@ -0,0 +1,17 @@
+const Cart = require("../models/Cart");
+const Item = require("../models/Item");
+
+module.exports.get_cart_items = async (req, res) => {
+	const userId = req.params.id;
+	try {
+		let cart = await Cart.findOne({ userId });
+		if (cart && cart.items.length > 0) {
+			res.send(cart);
+		} else {
+			res.send(null);
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Something went wrong");
+	}
+};
