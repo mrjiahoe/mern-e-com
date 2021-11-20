660a69b6804d591a08471b7349f7df2e1bc83cf8
jiahoe <mrjiahoe@hotmail.com>
2021-11-20 14:39:17 +0800

    create auth controllers

---
+++ controllers/authControllers.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcrypt");
+
+module.exports.signup = (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		res.status(400).json({ msg: "Please enter all fields" });
+	}
+
+	User.findOne({ email }).then((user) => {
+		if (user) return res.status(400).json({ msg: "User already exists" });
+
+		const newUser = new User({ name, email, password });
+
+		// Create salt and hash
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(password, salt, (err, hash) => {
+				if (err) throw err;
+				newUser.password = hash;
+				newUser.save().then((user) => {
+					jwt.sign(
+						{ id: user.id },
+						config.get("jwtsecret"),
+						{ expiresIn: 3600 },
+						(err, token) => {
+							if (err) throw err;
+							res.json({
+								token,
+								user: {
+									id: user._id,
+									name: user.name,
+									email: user.email,
+								},
+							});
+						}
+					);
+				});
+			});
+		});
+	});
+};
